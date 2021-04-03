2bca5c4179dc310e9947dc852c7a4ea23bcad108
Rugved <rugved.bongale@somaiya.edu>
2021-04-03 23:04:51 +0530

    saved variables for quiz form

--- src/App.js
+++ src/App.js
@@ -9,12 +9,16 @@ import {
 } from "react-router-dom";
 import Quiz from "./components/Quiz/Quiz";
 import Auth from "./components/Auth/Auth";
+import CustomModal from "./components/CustomModal/CustomModal";
 
 function App() {
   return (
     <div>
       <Router>
         <Switch>
+          <Route path="/modal">
+            <CustomModal />
+          </Route>
           <Route path="/quiz">
             <Quiz />
           </Route>
